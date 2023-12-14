07965cdce671d751efdc964744f7e97858f6f60a
mre55 <misganawmihreteab>
2023-12-14 15:01:41 +0300

    Add NavBar and no-projects image to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import NavBar from "./components/NavBar";
+import noProjects from './assets/no-projects.png';
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <div className="relative min-h-screen">
+      <section className="absolute top-1/4 left-2/3">
+        <img src={noProjects} alt="no-projects" className="h-24" />
+        <h2>No Project Selected</h2>
+        <p>No Project Selected</p>
+        <button>Create new project</button>
+      </section>
+      {/* position: absolute;
+  top: 50%;
+  left: 50%;
+  margin: -25px 0 0 -25px; */}
+      <NavBar />
+    </div>
   );
 }
 
