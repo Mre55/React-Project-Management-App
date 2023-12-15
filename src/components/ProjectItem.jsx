f98818b9eedbdff1d57ba86697bd4a9c112ceefc
mre55 <misganawmihreteab>
2023-12-15 14:39:58 +0300

    Add tasks inside ProjectItem component

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -17,16 +17,25 @@ const ProjectItem = () => {
         <button className="text-lg text-[#626060]">Delete</button>
       </div>
 
-      <h3>Tasks</h3>
+      <h3 className="mb-4 text-3xl font-bold text-[#4e4e4e]">Tasks</h3>
       <form className="flex items-center gap-x-12">
         <input
           type="text"
-          className="mt-2.5 pl-1 w-5/12 mb-5 h-10 bg-[#d4cfc485] text-[#77736a] border-b-[#302f2d]-500 rounded outline-none"
+          className="mt-2.5 pl-1 w-5/12 mb-5 h-10 bg-[#e1e0e0] text-[#77736a] border-b-[#302f2d]-500 rounded outline-none"
         />
-        <button className="m-0 text-lg tracking-wide text-[#4e4e4e]">
+        <button className="m-0 text-xl tracking-wide text-[#4e4e4e]">
           Add Task
         </button>
       </form>
+
+      <div className="bg-[#ebecedb3] py-7 px-4 rounded-md mt-7">
+        <p className="flex justify-between my-1.5 text-lg">
+          <span>Practic, practic, practice!</span> <button>Clear</button>
+        </p>
+        <p className="flex justify-between my-1.5 text-lg">
+          <span>Learn advanced concepts</span> <button>Clear</button>
+        </p>
+      </div>
     </section>
   );
 };
