309b30fa14524cb45b513cd673ee9a5808b465bc
mre55 <misganawmihreteab>
2023-12-15 14:47:54 +0300

    Edit HomePage style

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,13 +4,13 @@ const HomePage = () => {
   return (
     <div className="flex flex-col items-center absolute top-40 right-1/3">
       <img src={noProjects} alt="no-projects" className="h-24" />
-      <h2 className="font-bold my-5 text-2xl tracking-wide text-[#574b33]">
+      <h2 className="font-bold my-5 text-3xl tracking-wide text-[#837967]">
         No Project Selected
       </h2>
-      <p className="text-[#71654d] mb-12">
+      <p className="text-lg text-[#acaaa8] mb-12">
         Select a project or get started with a new one
       </p>
-      <button className="h-12 w-52 rounded-lg text-lg font-normal bg-[#3c3016] text-stone-300/75">
+      <button className="h-12 w-52 rounded-lg text-lg font-normal bg-[#392d13] text-stone-300/75">
         Create new project
       </button>
     </div>
