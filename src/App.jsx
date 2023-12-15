c1c220f4649726696b2dfcff5e3e9b2f52413060
mre55 <misganawmihreteab>
2023-12-15 14:17:36 +0300

    Render ProjectItem component in App js

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import NavBar from "./components/NavBar";
 import NewProject from "./components/NewProject";
 import NoProject from "./components/NoProject";
+import ProjectItem from "./components/ProjectItem";
 
 function App() {
   return (
     <div className="relative min-h-screen">
       {/* <NoProject /> */}
-      <NewProject />
+      {/* <NewProject /> */}
+      <ProjectItem />
       <NavBar />
     </div>
   );
