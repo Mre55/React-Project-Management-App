e96b52b7875e43182eafe3a8bdfa8482faf5c7e0
mre55 <misganawmihreteab>
2023-12-15 14:42:58 +0300

    Render HomePage on default view

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import NavBar from "./components/NavBar";
 import NewProject from "./components/NewProject";
-import NoProject from "./components/NoProject";
+import HomePage from "./components/HomePage";
 import ProjectItem from "./components/ProjectItem";
 
 function App() {
   return (
     <div className="relative min-h-screen">
-      {/* <NoProject /> */}
-      {/* <NewProject /> */}
-      <ProjectItem />
       <NavBar />
+      <HomePage />
+      {/* <NewProject /> */}
+      {/* <ProjectItem /> */}
     </div>
   );
 }
