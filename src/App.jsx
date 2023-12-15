9465bf59ae541f531f61b4b7c7d50e51a5d146d5
mre55 <misganawmihreteab>
2023-12-15 12:30:07 +0300

    Refactor App component to use NoProject component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
 import NavBar from "./components/NavBar";
-import noProjects from "./assets/no-projects.png";
+import NoProject from "./components/NoProject";
 
 function App() {
   return (
     <div className="relative min-h-screen">
-      <section className="flex flex-col items-center absolute top-40 right-1/3">
-        <img src={noProjects} alt="no-projects" className="h-24" />
-        <h2 className="font-bold my-5 text-2xl tracking-wide text-[#574b33]">
-          No Project Selected
-        </h2>
-        <p className="text-[#71654d] mb-12">
-          Select a project or get started with a new one
-        </p>
-        <button className="h-12 w-52 rounded-lg text-lg font-normal bg-[#3c3016] text-stone-300/75">
-          Create new project
-        </button>
-      </section>
+      <NoProject />
       <NavBar />
     </div>
   );
