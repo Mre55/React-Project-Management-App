b6942a62cdfccff24b1b302228604f8ddb0c1955
mre55 <misganawmihreteab>
2023-12-15 15:20:19 +0300

    Open ProjectItem component when selecting project from sidebar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 
-const NavBar = () => {
+const NavBar = ({ onAdd, onOpenProject }) => {
   return (
     <section className="pt-20 pl-12 absolute top-10 left-0 bg-zinc-900 rounded-tr-3xl min-h-screen w-1/4 text-gray-300">
       <div>
         <h2 className="font-semibold text-2xl tracking-wide">YOUR PROJECTS</h2>
-        <button className="mt-16 h-12 w-40 rounded-lg text-lg font-normal bg-amber-100/[.1] text-stone-300/75">
+        <button
+          className="mt-16 h-12 w-40 rounded-lg text-lg font-normal bg-amber-100/[.1] text-stone-300/75"
+          onClick={onAdd}
+        >
           + Add Project
         </button>
       </div>
       <div className="mt-12">
-        <p className="my-2.5 text-lg text-[#bbb9b5]">Learning React</p>
-        <p className="my-2.5 text-lg text-[#bbb9b5]">Mastering React</p>
+        <ul>
+          <li className="my-2.5 text-lg text-[#bbb9b5]">
+            <button onClick={onOpenProject}>Learning React</button>
+          </li>
+          <li className="my-2.5 text-lg text-[#bbb9b5]">
+            <button onClick={onOpenProject}>Mastering React</button>
+          </li>
+        </ul>
       </div>
     </section>
   );
