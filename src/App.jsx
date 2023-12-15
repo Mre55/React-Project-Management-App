22fe64cf4cca64b8f9919757ce30d798bc07a122
mre55 <misganawmihreteab>
2023-12-15 15:21:01 +0300

    Render pages conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
+import { useState } from "react";
+
 import NavBar from "./components/NavBar";
-import NewProject from "./components/NewProject";
 import HomePage from "./components/HomePage";
+import NewProject from "./components/NewProject";
 import ProjectItem from "./components/ProjectItem";
 
 function App() {
+  const [mainPage, setMainPage] = useState({
+    homePage: true,
+    newProject: false,
+    projectItem: false,
+  });
+
+  const handleAddProject = () => {
+    setMainPage((prevPage) => {
+      return { ...!prevPage, newProject: true };
+    });
+  };
+
+  const handleOpenProject = () => {
+    setMainPage((prevPage) => {
+      return { ...!prevPage, projectItem: true };
+    });
+  };
+
   return (
     <div className="relative min-h-screen">
-      <NavBar />
-      <HomePage />
-      {/* <NewProject /> */}
-      {/* <ProjectItem /> */}
+      <NavBar onAdd={handleAddProject} onOpenProject={handleOpenProject} />
+      {mainPage.homePage && <HomePage onAdd={handleAddProject} />}
+      {mainPage.newProject && <NewProject />}
+      {mainPage.projectItem && <ProjectItem />}
     </div>
   );
 }
