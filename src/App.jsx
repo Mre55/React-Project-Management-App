6d3c50eedbb9b93aa52a40be3f1c01cac7431faf
mre55 <misganawmihreteab>
2023-12-15 12:20:18 +0300

    Refactor App component layout and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import NavBar from "./components/NavBar";
-import noProjects from './assets/no-projects.png';
+import noProjects from "./assets/no-projects.png";
 
 function App() {
   return (
     <div className="relative min-h-screen">
-      <section className="absolute top-1/4 left-2/3">
+      <section className="flex flex-col items-center absolute top-40 right-1/3">
         <img src={noProjects} alt="no-projects" className="h-24" />
-        <h2>No Project Selected</h2>
-        <p>No Project Selected</p>
-        <button>Create new project</button>
+        <h2 className="font-bold my-5 text-2xl tracking-wide text-[#574b33]">
+          No Project Selected
+        </h2>
+        <p className="text-[#71654d] mb-12">
+          Select a project or get started with a new one
+        </p>
+        <button className="h-12 w-52 rounded-lg text-lg font-normal bg-[#3c3016] text-stone-300/75">
+          Create new project
+        </button>
       </section>
-      {/* position: absolute;
-  top: 50%;
-  left: 50%;
-  margin: -25px 0 0 -25px; */}
       <NavBar />
     </div>
   );
