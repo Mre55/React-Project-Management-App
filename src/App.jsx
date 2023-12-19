5134463e6f73e84826b101c6b6ae45b8f73c3d3e
mre55 <misganawmihreteab>
2023-12-19 15:00:50 +0300

    Add dummy items

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,23 @@ import HomePage from "./components/HomePage";
 import NewProject from "./components/NewProject";
 import ProjectItem from "./components/ProjectItem";
 
+const DUMMY_ITEMS = [
+  {
+    id: "d1",
+    title: "Learning React",
+    description: "Learn React from the group up.",
+    date: "Dec 29, 2024",
+    tasks: ["Practic, practic, practice!", "Learn advanced concepts"],
+  },
+  {
+    id: "d2",
+    title: "Mastering React",
+    description: "Master React until the top.",
+    date: "Feb 12, 2024",
+    tasks: ["Master by practicing!", "Master advanced concepts"],
+  },
+];
+
 function App() {
   const [mainPage, setMainPage] = useState({
     homePage: true,
@@ -26,7 +43,11 @@ function App() {
 
   return (
     <div className="relative min-h-screen">
-      <NavBar onAdd={handleAddProject} onOpenProject={handleOpenProject} />
+      <NavBar
+        onAdd={handleAddProject}
+        onOpenProject={handleOpenProject}
+        items={DUMMY_ITEMS}
+      />
       {mainPage.homePage && <HomePage onAdd={handleAddProject} />}
       {mainPage.newProject && <NewProject />}
       {mainPage.projectItem && <ProjectItem />}
