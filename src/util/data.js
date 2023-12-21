741b65ac50e90bc55789712c1f75678570676dd4
mre55 <misganawmihreteab>
2023-12-21 15:46:24 +0300

    Added new file for data

---
+++ src/util/data.js
@@ -0,0 +1,43 @@
+export const DUMMY_ITEMS = [
+  {
+    id: "d1",
+    title: "Learning React",
+    description:
+      "Learn React from the group up. Start with the basics, finish with advanced knowledge.",
+    date: "Dec 29, 2024",
+    tasks: [
+      {
+        id: "d1t1",
+        title: "Practic, practic, practice!",
+      },
+      {
+        id: "d1t2",
+        title: "Learn advanced concepts",
+      },
+      {
+        id: "d1t3",
+        title: "Twice practic, practic, practice!",
+      },
+    ],
+  },
+  {
+    id: "d2",
+    title: "Mastering React",
+    description: "Master React until the top.",
+    date: "Feb 12, 2024",
+    tasks: [
+      {
+        id: "d2t1",
+        title: "Master by practicing!",
+      },
+      {
+        id: "d2t2",
+        title: "Master advanced concepts",
+      },
+      {
+        id: "d2t3",
+        title: "Twice practic, practic, practice!",
+      },
+    ],
+  },
+];
