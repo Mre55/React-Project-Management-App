d94d37647777412ad6a0113980cf6ec4ed169bab
mre55 <misganawmihreteab>
2024-01-08 14:58:34 +0300

    Open New Project on Add project button clicked

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,13 +1,13 @@
 import Button from "./Button";
 
-const ProjectsSidebar = ({ onAdd, onOpenProject, items }) => {
+const ProjectsSidebar = ({ onStartAddProject, onOpenProject, items }) => {
   return (
     <aside className="px-8 py-16 bg-stone-900 w-1/3 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
         Your Projects
       </h2>
       <div>
-        <Button onClick={onAdd}>+ Add Project</Button>
+        <Button onClick={onStartAddProject}>+ Add Project</Button>
       </div>
       <ul>
         {items.map((item) => (
