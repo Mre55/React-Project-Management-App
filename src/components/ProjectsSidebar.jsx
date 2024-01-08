8c181df11c4a39fa72f31df184982a8c82bac9c1
mre55 <misganawmihreteab>
2024-01-08 14:44:31 +0300

    Render newly created Button to ProjectSidebar

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,3 +1,5 @@
+import Button from "./Button";
+
 const ProjectsSidebar = ({ onAdd, onOpenProject, items }) => {
   return (
     <aside className="px-8 py-16 bg-stone-900 w-1/3 text-stone-50 md:w-72 rounded-r-xl">
@@ -5,12 +7,7 @@ const ProjectsSidebar = ({ onAdd, onOpenProject, items }) => {
         Your Projects
       </h2>
       <div>
-        <button
-          className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100"
-          onClick={onAdd}
-        >
-          + Add Project
-        </button>
+        <Button onClick={onAdd}>+ Add Project</Button>
       </div>
       <ul>
         {items.map((item) => (
