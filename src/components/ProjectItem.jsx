5faefad55b6c84cdd1f0a303bf7673caa8e6f8d4
mre55 <misganawmihreteab>
2024-01-08 13:49:43 +0300

    Add filter to filter project with ID

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,11 +1,11 @@
 import React from "react";
+import NewTask from "./NewTask";
 
-const ProjectItem = ({ items, projectId }) => {
-  const filteredItems = items.filter((item) => item.id === projectId);
+const ProjectItem = ({ items }) => {
 
   return (
     <section className="absolute top-36 left-1/4 ml-12 w-7/12">
-      {filteredItems.map((item) => (
+      {items.map((item) => (
         <div key={item.id}>
           <div className="flex justify-between items-start mb-8 gap-x-5 border-b-2 border-b-[#e3e2df]">
             <div>
@@ -19,23 +19,20 @@ const ProjectItem = ({ items, projectId }) => {
           </div>
 
           <h3 className="mb-4 text-3xl font-bold text-[#4e4e4e]">Tasks</h3>
-          <form className="flex items-center gap-x-12">
-            <input
-              type="text"
-              className="mt-2.5 pl-1 w-5/12 mb-5 h-10 bg-[#e1e0e0] text-[#77736a] border-b-[#302f2d]-500 rounded outline-none"
-            />
-            <button className="m-0 text-xl tracking-wide text-[#4e4e4e]">
-              Add Task
-            </button>
-          </form>
+          <NewTask />
 
-          <div className="bg-[#ebecedb3] py-7 px-4 rounded-md mt-7">
-            {item.tasks.map((task) => (
-              <p key={task.id} className="flex justify-between my-1.5 text-lg">
-                <span>{task.title}</span> <button>Clear</button>
-              </p>
-            ))}
-          </div>
+          {item.tasks && (
+            <div className="bg-[#ebecedb3] py-7 px-4 rounded-md mt-7">
+              {item.tasks.map((task) => (
+                <p
+                  key={task.id}
+                  className="flex justify-between my-1.5 text-lg"
+                >
+                  <span>{task.title}</span> <button>Clear</button>
+                </p>
+              ))}
+            </div>
+          )}
         </div>
       ))}
     </section>
