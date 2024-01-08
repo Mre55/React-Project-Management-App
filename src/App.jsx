05a99c56011341f552c2a315e43cefdaec1e70b0
mre55 <misganawmihreteab>
2024-01-08 13:49:13 +0300

    Import ProjectSidebar component in App js

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,53 @@
 import { useState } from "react";
 
-import NavBar from "./components/NavBar";
+import ProjectsSidebar from "./components/ProjectsSideBar";
 import HomePage from "./components/HomePage";
 import NewProject from "./components/NewProject";
 import ProjectItem from "./components/ProjectItem";
 import { DUMMY_ITEMS } from "./util/data";
 
 function App() {
+  const [items, setItems] = useState(DUMMY_ITEMS);
+
   const [mainPage, setMainPage] = useState({
     homePage: true,
     newProject: false,
-    projectItem: false,
+    projectItem: [],
   });
 
-  const [selectedProjectId, setSelectedProjectId] = useState("");
-
   const handleNewProject = ({ id, title, description, date }) => {
-    console.log("the new project id is ", id);
-    console.log("the new project title is ", title);
-    console.log("the new project description is ", description);
-    console.log("the new project date is ", date);
+    setItems((prevItems) => {
+      return [...prevItems, { id, title, description, date }];
+    });
   };
 
   const handleAddProject = () => {
-    setMainPage((prevPage) => {
-      return { ...!prevPage, newProject: true };
-    });
+    console.log("add project clicked!", mainPage.newProject);
+    setMainPage({ newProject: true });
+    // setMainPage((prevPage) => {
+    //   return { ...!prevPage, newProject: true };
+    // });
   };
 
   const handleOpenProject = (id) => {
-    setMainPage((prevPage) => {
-      return { ...!prevPage, projectItem: true };
-    });
+    const openFilteredItem = items.filter((item) => item.id === id);
+
+    setMainPage({ projectItem: openFilteredItem });
     setSelectedProjectId(id);
   };
 
   return (
-    <div className="relative min-h-screen">
-      <NavBar
+    <main className="min-h-screen my-8">
+      <ProjectsSidebar
         onAdd={handleAddProject}
         onOpenProject={handleOpenProject}
-        items={DUMMY_ITEMS}
+        items={items}
       />
       {mainPage.homePage && <HomePage onAdd={handleAddProject} />}
       {mainPage.newProject && <NewProject onSave={handleNewProject} />}
-      {mainPage.projectItem && (
-        <ProjectItem items={DUMMY_ITEMS} projectId={selectedProjectId} />
-      )}
-    </div>
+
+      <ProjectItem items={mainPage.projectItem} />
+    </main>
   );
 }
 
