216c02cf7af60735b8c589a766b8547d9a165880
mre55 <misganawmihreteab>
2024-01-08 14:59:37 +0300

    Render NewProject and NoProjectSelected conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,9 @@ import NoProjectSelected from "./components/NoProjectSelected";
 function App() {
   const [items, setItems] = useState(DUMMY_ITEMS);
 
-  const [mainPage, setMainPage] = useState({
-    homePage: true,
-    newProject: false,
-    projectItem: [],
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
   });
 
   const handleNewProject = ({ id, title, description, date }) => {
@@ -22,12 +21,13 @@ function App() {
     });
   };
 
-  const handleAddProject = () => {
-    console.log("add project clicked!", mainPage.newProject);
-    setMainPage({ newProject: true });
-    // setMainPage((prevPage) => {
-    //   return { ...!prevPage, newProject: true };
-    // });
+  const handleStartAddProject = () => {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
   };
 
   const handleOpenProject = (id) => {
@@ -37,19 +37,22 @@ function App() {
     setSelectedProjectId(id);
   };
 
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="min-h-screen my-8 flex gap-8">
       <ProjectsSidebar
-        onAdd={handleAddProject}
+        onStartAddProject={handleStartAddProject}
         onOpenProject={handleOpenProject}
         items={items}
       />
-      <NoProjectSelected />
-      {/* <NewProject /> */}
-      {/* {mainPage.homePage && <HomePage onAdd={handleAddProject} />}
-      {mainPage.newProject && <NewProject onSave={handleNewProject} />}
-
-      <ProjectItem items={mainPage.projectItem} /> */}
+      {content}
     </main>
   );
 }
