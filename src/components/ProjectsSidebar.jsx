6b33274d55a3ab6aaf82b7e585300651c39af7a2
mre55 <misganawmihreteab>
2024-01-09 13:34:09 +0300

    Render project title at side bar

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,6 +1,6 @@
 import Button from "./Button";
 
-const ProjectsSidebar = ({ onStartAddProject, onOpenProject, items }) => {
+const ProjectsSidebar = ({ onStartAddProject, projects }) => {
   return (
     <aside className="px-8 py-16 bg-stone-900 w-1/3 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
@@ -9,10 +9,12 @@ const ProjectsSidebar = ({ onStartAddProject, onOpenProject, items }) => {
       <div>
         <Button onClick={onStartAddProject}>+ Add Project</Button>
       </div>
-      <ul>
-        {items.map((item) => (
-          <li key={item.id} className="my-2.5 text-lg text-[#bbb9b5]">
-            <button onClick={() => onOpenProject(item.id)}>{item.title}</button>
+      <ul className="mt-8">
+        {projects.map((project) => (
+          <li key={project.id}>
+            <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800">
+              {project.title}
+            </button>
           </li>
         ))}
       </ul>
