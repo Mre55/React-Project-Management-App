1d5ad11803a1fd813517deff28dea41f890364e2
mre55 <misganawmihreteab>
2024-01-09 14:25:54 +0300

    Close New Project component when cancel button clicked

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 import Input from "./Input";
 import Modal from "./Modal";
 
-const NewProject = ({ onAdd }) => {
+const NewProject = ({ onAdd, onCancel }) => {
   const modal = useRef();
 
   const title = useRef();
@@ -38,13 +38,20 @@ const NewProject = ({ onAdd }) => {
     <>
       <Modal ref={modal} buttonCaption="Okay">
         <h2 className="font-bold my-4 text-xl text-stone-700">Invalid Input</h2>
-        <p className="text-stone-600 mb-4">Oops ... looks like you forgot to enter a value.</p>
-        <p className="text-stone-600 mb-4">Please make sure you provide a valid value for every input field.</p>
+        <p className="text-stone-600 mb-4">
+          Oops ... looks like you forgot to enter a value.
+        </p>
+        <p className="text-stone-600 mb-4">
+          Please make sure you provide a valid value for every input field.
+        </p>
       </Modal>
       <div className="w-[35rem] mt-16">
         <menu className="flex items-center justify-end gap-4 my-4">
           <li>
-            <button className="text-stone-800 hover:text-stone-950">
+            <button
+              onClick={onCancel}
+              className="text-stone-800 hover:text-stone-950"
+            >
               Cancel
             </button>
           </li>
