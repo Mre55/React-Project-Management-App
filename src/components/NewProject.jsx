d8ad0f871b869775b15c70eeadb8bc8390a999e3
mre55 <misganawmihreteab>
2024-01-09 14:13:26 +0300

    Render Modal component when user enter invalid input

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,7 +1,10 @@
-import React, { useState, useRef } from "react";
+import React, { useRef } from "react";
 import Input from "./Input";
+import Modal from "./Modal";
 
 const NewProject = ({ onAdd }) => {
+  const modal = useRef();
+
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
@@ -15,6 +18,15 @@ const NewProject = ({ onAdd }) => {
 
     // validation...
 
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescription.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ) {
+      modal.current.open();
+      return;
+    }
+
     onAdd({
       title: enteredTitle,
       description: enteredDescription,
@@ -23,76 +35,35 @@ const NewProject = ({ onAdd }) => {
   };
 
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button
-            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-            onClick={handleSave}
-          >
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input type="text" ref={title} label={"Title"} />
-        <Input ref={description} label={"Description"} textarea />
-        <Input type="date" ref={dueDate} label={"Due Date"} />
+    <>
+      <Modal ref={modal} buttonCaption="Okay">
+        <h2>Invalid Input</h2>
+        <p>Oops ... looks like you forgot to enter a value.</p>
+        <p>Please make sure you provide a valid value for every input field.</p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950">
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" ref={title} label={"Title"} />
+          <Input ref={description} label={"Description"} textarea />
+          <Input type="date" ref={dueDate} label={"Due Date"} />
+        </div>
       </div>
-      {/* <form
-      className="absolute top-36 left-1/4 ml-12 w-7/12"
-      onSubmit={handleSubmit}
-    >
-      <div className="flex justify-end mb-8 gap-x-5">
-        <button className="text-lg text-[#302f2d]">Cancel</button>
-        <button className="h-12 w-24 rounded-lg text-lg font-normal bg-[#272521] text-[#fff]">
-          Save
-        </button>
-      </div>
-      <div className="flex flex-col">
-        <label
-          htmlFor="title"
-          className="m-0 font-bold text-lg tracking-wide text-[#777369]"
-        >
-          TITLE
-        </label>
-        <input
-          ref={titleRef}
-          id="title"
-          type="text"
-          className="mt-2.5 pl-1 mb-5 h-10 bg-[#d4cfc466] text-[#77736a] border-b-[#302f2d]-500 rounded-tl rounded-tr border-2 border-b-[#77736a] outline-none"
-        />
-        <label
-          htmlFor="description"
-          className="m-0 font-bold text-lg tracking-wide text-[#777369]"
-        >
-          DESCRIPTION
-        </label>
-        <textarea
-          ref={descriptionRef}
-          id="description"
-          className="mt-2.5 mb-5 pl-1 h-20 bg-[#d4cfc466] text-[#77736a] border-b-[#302f2d]-500 rounded-tl rounded-tr border-y border-b-[#d1d0cc] outline-none"
-        ></textarea>
-        <label
-          htmlFor="due-date"
-          className="m-0 font-bold text-lg tracking-wide text-[#777369]"
-        >
-          DUE DATE
-        </label>
-        <input
-          ref={dateInputRef}
-          id="due-date"
-          type="date"
-          className="mt-2.5 mb-5 h-10 bg-[#d4cfc466] text-[#77736a] border-b-[#302f2d]-500 rounded-tl rounded-tr border-2 border-b-[#77736a] outline-none"
-        />
-      </div>
-    </form> */}
-    </div>
+    </>
   );
 };
 
